3c28b79d7a5668cd582c3740ae5a63ed87b91489
Piotr Dyba <piotr@dyba.com.pl>
2015-05-20 16:48:30 +0200

    wazne dane

---
+++ sample.py
@@ -0,0 +1,32 @@
+nokaut_api_params = urllib.urlencode({'key':str(nokaut_key),'keyword':str(keyword),'format':'xml','method': 'nokaut.Product.getByKeyword','sort_direction': 'price_asc', 'returnType': 'full', 'limit':'1'})
+
+abc = dict(abc=2, cde=5, mama='tata', tata='mama', raz=1, dwa=2)
+
+data = {model_field: form[form_field].data for form_field, model_field in fields if form_field in form}
+
+if alicja in kraina_czarow and przemek in dom or aleksandra in praca and pogoda is ladna or 2+2==5 and glaz:
+		print('a')
+
+abc = qwe(product.to_dict(),cls=DecimalEncoder,mimetype='application/json',mama=3, tata=3)
+
+class ala:
+	cos tam
+class tomek_xxx:
+	meta = ala
+	def ppp():
+		return a
+	def pwwwpp():
+		return w
+def PoKUsA():
+		return adads
+def hiPPo_OOOtam():
+	return hip
+
+
+
+
+
+
+
+
+import urllib
